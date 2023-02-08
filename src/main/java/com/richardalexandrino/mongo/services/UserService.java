5b8d6f5f27ebeeef0d70ee7d1541e99a9aa62081
Richard Alexandrino <richardalexandrino@hotmail.com>
2023-02-08 17:31:40 -0300

    User insertion with POST

--- src/main/java/com/richardalexandrino/mongo/services/UserService.java
+++ src/main/java/com/richardalexandrino/mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.richardalexandrino.mongo.domain.User;
+import com.richardalexandrino.mongo.dto.UserDTO;
 import com.richardalexandrino.mongo.repository.UserRepository;
 import com.richardalexandrino.mongo.services.exception.ObjectNotFoundException;
 
@@ -23,6 +24,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return repository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 	
 }
